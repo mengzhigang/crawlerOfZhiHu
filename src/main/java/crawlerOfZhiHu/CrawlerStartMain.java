0f6aa60427d8aeb9894756ba1455c9a48633309e
mzg96 <mzg96@DESKTOP-1J36IDK>
2018-03-13 14:18:26 +0800

    提交

---
+++ src/main/java/crawlerOfZhiHu/CrawlerStartMain.java
@@ -0,0 +1,11 @@
+package crawlerOfZhiHu;
+
+public class CrawlerStartMain {
+
+	public static void main(String[] args) {
+		System.out.println("-----------------解析开始-------------------------");
+		System.out.println("1.解析配置文件");
+		
+		System.out.println("-----------------解析结束-------------------------");
+	}
+}
---
+++ src/main/java/crawlerOfZhiHu/utils/ConfigPraseUtils.java
@@ -0,0 +1,5 @@
+package crawlerOfZhiHu.utils;
+
+public class ConfigPraseUtils {
+
+}
